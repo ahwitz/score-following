6da747c8e0210e5e091175e3d28ca7e19d79f9e0
Andrew Horwitz <ahwitz@gmail.com>
2015-01-23 17:43:47 -0500

    WIP: playback canvas with horizontal line for position in track
    -Added canvas for overlay with current playback position.
    
    Not fully functional yet.

--- audio.js
+++ audio.js
@@ -25,72 +25,81 @@ var audioSource;
 
 //Canvas settings/vars
 var canvasWidth = $(window).width() - 20,  canvasHeight = 120 ;
-var newCanvas   = createCanvas (canvasWidth, canvasHeight);
-var canvasContext;
+var wCanvas, wCanvasContext; //waveform canvas
+var pCanvas, pCanvasContext; //playback overlay canvas
+var pCanvasAdvanceInterval, pCanvasPos; //variables for animation
 var samplesPerPixel;
 
 //Other
 var errorTimeout;
 var ERROR_TIMEOUT_TIMER = 5000;
+var SAMPLE_RATE;
 
 function initSound(arrayBuffer) {
     audioContext.decodeAudioData(arrayBuffer, function(buffer) {
         // audioBuffer is global to reuse the decoded audio later.
         audioBuffer = buffer;
+        SAMPLE_RATE = buffer.sampleRate;
         renderCanvas();
+
+        pCanvas = createCanvas(canvasWidth, canvasHeight, "playback-canvas");
+        pCanvas.style.position = "fixed";
+        pCanvas.style.zIndex = wCanvas.style.zIndex + 1;
+        $("#playback-canvas").offset($("#waveform-canvas").offset());
+        pCanvasContext = pCanvas.getContext('2d');
+        pCanvasContext.fillStyle = 'rgba(0, 0, 0, 1)';
+        pCanvasContext.fillRect(0,0,canvasWidth,canvasHeight);
+        pCanvasContext.strokeStyle = '#FF0000';
+        pCanvasContext.globalCompositeOperation = 'lighter';
     }, function(e) {
         console.log('Error decoding file', e);
     }); 
 }
 
-// MUSIC DISPLAY
+//display waveform
 function renderCanvas() 
 {
-    console.log('called');
     var leftChannel = audioBuffer.getChannelData(0); // Float32Array describing left channel 
-    var rightChannel = audioBuffer.getChannelData(1); // Float32Array describing right channel 
-    var samplesPerPixel = leftChannel.length / canvasWidth;    
-    canvasContext.save();
-    canvasContext.fillStyle = '#CCCCCC' ;
-    canvasContext.fillRect(0,0,canvasWidth,canvasHeight );
-    canvasContext.strokeStyle = '#00FF00';
-    canvasContext.globalCompositeOperation = 'darker';
-    canvasContext.translate(0,canvasHeight / 2);
+    var samplesPerPixel = leftChannel.length / canvasWidth;
 
-    for (var x=0; x < canvasWidth; x++) {
-        var yMax = 0, yMin = 0;
+    wCanvasContext.save();
+    wCanvasContext.fillStyle = '#CCCCCC' ;
+    wCanvasContext.fillRect(0,0,canvasWidth,canvasHeight );
+    wCanvasContext.strokeStyle = '#00FF00';
+    wCanvasContext.globalCompositeOperation = 'darker';
+    wCanvasContext.translate(0,canvasHeight / 2);
 
-        for (var j=0; j < samplesPerPixel; j++){
-            yMax = Math.max(yMax, leftChannel[canvasWidth*x + j]);
-            yMin = Math.max(yMin, leftChannel[canvasWidth*x + j]);
-        }
-
-        yMax = yMax * canvasHeight / 2 ;
-        yMin = -(yMin * canvasHeight / 2 );
-
-        canvasContext.beginPath();
-        canvasContext.moveTo( x, yMin );
-        canvasContext.lineTo( x, yMax );
-        canvasContext.stroke();
+    for (var i=0; i < leftChannel.length; i+=50)
+    {
+        var x = Math.floor ( canvasWidth * i / leftChannel.length ) ;
+        var y = leftChannel[i] * canvasHeight / 2 ;
+        wCanvasContext.beginPath();
+        wCanvasContext.moveTo( x  , 0 );
+        wCanvasContext.lineTo( x+1, y );
+        wCanvasContext.stroke();
     }
-    canvasContext.restore();
+
+    wCanvasContext.restore();
     console.log('done');
-    $("#click").remove();
-    $("#playback").css('display', 'block');
 }
 
-function createCanvas ( w, h ) 
+function createCanvas ( w, h, id ) 
 {
-    var newCanvas = document.createElement('canvas');
-    newCanvas.width  = w;
-    newCanvas.height = h;
-    return newCanvas;
+    $("body").append("<canvas id='" + id + "'></canvas>");
+    var tempCanvas = document.getElementById(id);
+    console.log("making a new canvas", tempCanvas);
+    tempCanvas.width  = w;
+    tempCanvas.height = h;
+    return tempCanvas;
 }
 
 function writeError (text)
 {
     $("#error").text(text);
-    errorTimeout = setTimeout(function(){ $("#error").text(""); }, ERROR_TIMEOUT_TIMER);
+    errorTimeout = setTimeout(function()
+    { 
+        $("#error").text(""); 
+    }, ERROR_TIMEOUT_TIMER);
 }
 
 $(window).on('load', function(e)
@@ -119,6 +128,22 @@ $(window).on('load', function(e)
             return;
         }
 
+        pCanvasContext.beginPath();
+        pCanvasPos = 0;
+        pCanvasContext.moveTo( pCanvasPos  , 0 );
+        pCanvasContext.lineTo( pCanvasPos, canvasHeight );
+        pCanvasContext.stroke();
+
+        pCanvasAdvanceInterval = setInterval(function(e)
+        {
+            pCanvasContext.clearRect(pCanvasPos, 0, pCanvasPos, canvasHeight);
+            pCanvasPos++;
+            pCanvasContext.beginPath();
+            pCanvasContext.moveTo( pCanvasPos  , 0 );
+            pCanvasContext.lineTo( pCanvasPos, canvasHeight );
+            pCanvasContext.stroke();            
+        }, samplesPerPixel / SAMPLE_RATE);
+
         audioSource = audioContext.createBufferSource();
         audioSource.buffer = audioBuffer;
         audioSource.loop = false;
@@ -133,19 +158,23 @@ $(window).on('load', function(e)
 
     $("#pause-button").on('click', function()
     {
-        if (audioSource !== undefined && audioSource.isPlaying === true) {
-            audioSource.isPlaying = false;
-            audioSource.stop(0);
-        } 
-        else
+        if (audioSource === undefined || audioSource.isPlaying === false) 
         {
             writeError("Source is not playing.");
             return;
         }
+        
+        audioSource.isPlaying = false;
+        audioSource.stop(0);
+        
+        clearInterval(pCanvasAdvanceInterval);
+        $("#playback-canvas").remove();
+        pCanvas = null;
+        pCanvasContext = null;
+        pCanvasAdvanceInterval = null;
     });
 
-    newCanvas.id = "waveform-canvas";
-    document.body.appendChild(newCanvas);
-    canvasContext = newCanvas.getContext('2d'); 
+    wCanvas = createCanvas(canvasWidth, canvasHeight, "waveform-canvas"); //waveform canvas
+    wCanvasContext = wCanvas.getContext('2d'); 
 });
 
