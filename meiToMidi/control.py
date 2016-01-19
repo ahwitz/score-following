67387f1e85e5eded83001bc18c28df837dace52b
Andrew Horwitz <ahwitz@gmail.com>
2016-01-18 22:11:40 -0500

    Complete refactor -Split s_f_utils into s_f_classes as well

--- meiToMidi/control.py
+++ meiToMidi/control.py
@@ -14,88 +14,90 @@ from scipy.fftpack import fft
 # my code
 from midi_fourier import *
 from s_f_utils import *
+from s_f_classes import *
 
 # debugging
 import pprint
 import pdb
 import sys
 
+# command-line arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--mei', help='mei file to align', required=True)
 parser.add_argument('--audio', help='audio file to align')
-
 args = parser.parse_args()
 mei_file = args.mei
 audio_file = args.audio
-stem = 'salz-test-out'
-pp = pprint.PrettyPrinter(indent=4)
+new_file_stem = mei_file.split(".")[0] + "-out"
 
-MAX_FREQ = 300000000
-SMOOTH_DISTANCE = 5
-LOCAL_MAX_DISTANCE = 10
-wav_debug = False
-img_debug = False
-
-if img_debug:
-    import matplotlib.pyplot as plt
-
-# data to pull in from music21
-tempo = 0
-instruments = [] # list of MIDI program numbers
-instruments_fft = {} # eventually, fft data archived by midi_fourier.py
-
-parsed = converter.parseFile(mei_file, None, 'mei', True)
-timewise = timewiseMusic21(parsed)
-tempo = timewise.tempo
-# tempos = [z.secondsPerQuarter() for x, y, z in p.metronomeMarkBoundaries()] # I think this is when the piece has multiple tempos... I should comment my code better.
-
-#TODO: cli for this
+#TODO: cli for converting to wav and appending timeline info
 # write the midi, convert it to wav using Timidity
-# parsed.write('midi', stem + '.midi')
-# fp = open(stem + ".wav", "w")
-# p1 = Popen(["timidity", stem + ".midi", "-Ow"], stdout=fp)
+# parsed.write('midi', new_file_stem + '.midi')
+# fp = open(new_file_stem + ".wav", "w")
+# p1 = Popen(["timidity", new_file_stem + ".midi", "-Ow"], stdout=fp)
 # wait = p1.wait() #async, make sure file is completely done
 # fp.close()
 
+# Globals
+pp = pprint.PrettyPrinter(indent=4)
+MAX_FREQ = 300000000 # not actually that important, just needs to be large
+SMOOTH_DISTANCE = 5 # Hz to smooth out each peak by 
+LOCAL_MAX_DISTANCE = 10 # distance from each expected peak to search for the actual value
+NUM_PEAKS = 10 # number of top peaks to account for in each window
+wav_debug = False # debugs by printing out wavs of each window TODO: CLI
+img_debug = False # debugs by printing out waveform graphs of each window TODO: CLI
+if img_debug:
+    import matplotlib.pyplot as plt
+
+# setting various variables that will be used later
+seconds = audible_length / sample_rate # length of the piece in seconds
+window_length = sample_rate # length of the window to use TODO: CLI
+window_seconds = window_length / sample_rate # length of each window in seconds
+sample_offset = int(floor(audible_length / (seconds / tempo))) # distance between window beginnings TODO: CLI
+plot_length = min(window_length / 2, MAX_FREQ) # length of the fourier plot, window_length/2 because the fourier plot is mirrored
+
+# Parse MEI and WAV
+print ("Parsing MEI...")
+instruments_fft = {} # eventually, fft data archived by midi_fourier.py
+parsed = converter.parseFile(mei_file, None, 'mei', True) # parsed MEI file as a Music21 score
+timewise = timewiseMusic21(parsed) # local format
+tempo = timewise.tempo # default tempo of the piece
+# tempos = [z.secondsPerQuarter() for x, y, z in p.metronomeMarkBoundaries()] # When the piece has multiple tempos
+
 # load the pre-parsed instrument data
+instruments = [] # list of MIDI program numbers
 for part in parsed[1:]: # parsed[0] is metadata; we don't want that
     # TODO: have it parse new instruments if it doesn't have it stored yet, also keep track of what soundfont/MIDI library has been parsed, if this changes redo them
     this_instrument = part[0].getInstrument(returnDefault = True).midiProgram or 0 # it's part of the measure object (part[0]) for some reason; if it's None we just want 0 (piano)
 
-    instruments.append(this_instrument) 
-    instruments_fft[this_instrument] = loadInstrument(this_instrument) # from midi_fourier.py
+    if this_instrument not in instruments:
+        instruments.append(this_instrument) 
+        instruments_fft[this_instrument] = loadInstrument(this_instrument) # from midi_fourier.py
 
 # reload the wav, get the first track
-print("Reading wav.")
+print("Reading wav...")
 sample_rate, data = wavfile.read(audio_file)
 first_track = data.T[0] # first channel
 
-# chop off the spare -1 frames at the end
+# chop off any empty frames from the end
 audible_length = len(first_track) - 1
 while first_track[audible_length] == -1:
     audible_length -= 1
 
-# setting various variables that will be used later
-seconds = audible_length / sample_rate
-# quarters = int(floor(seconds / tempo)) # quarter notes in the piece, only used for window_length
-window_length = sample_rate # floor(audible_length / quarters)
-sample_offset = int(floor(audible_length / (seconds / tempo)))
-plot_length = min(window_length / 2, MAX_FREQ) # window_length/2 because the fourier plot is mirrored
-window_seconds = window_length / sample_rate 
+# Alignment time!
+start_point = 0 # current start frame
+count = 0 # nth window, used for debug
+events = {} # list of things that happened
+silent = True # tracks while the music is silent at the beginning
+print("Aligning with window size of", str(window_length) + "...")
 
-# pp.pprint(sorted(timewise.items(), key=operator.itemgetter(0)))
-print("Writing with", window_length, "frame window size: ")
-lastMidi = -1
-start_point = 0
-count = 0
-events = {}
-silent = True
 while start_point < audible_length: 
     # start_point is bumped up at the end of the loop
     end_point = start_point + window_length
     if end_point > audible_length:  # just in case
         break
 
+    # convert frames to seconds
     start_seconds = start_point / sample_rate
     end_seconds = end_point / sample_rate
 
@@ -109,7 +111,7 @@ while start_point < audible_length:
         wavfile.write('wavout/quarter' + str(count) + 'wav', sample_rate, track_subsection)
 
     # move everything from normalized frequency to Hz
-    hz_plot = [None] * int(ceil(normToHz(plot_length, window_seconds)))
+    hz_plot = [None] * int(ceil(normToHz(plot_length, window_seconds))) # 0 to highest hz for the x axis
     idx = 0
     for y in yf:
         hz = int(normToHz(idx, window_seconds))
@@ -119,7 +121,7 @@ while start_point < audible_length:
             hz_plot[hz].append(y)
         idx += 1
 
-    # if window_length > sample_rate, this will average everything that resolves to the same frequency, otherwise it condenses int arrays of length 1 to ints
+    # if multiple normalized frequencies lined up with the same integer Hz, average them
     idx = 0
     for arr in hz_plot:
         if arr == None:
@@ -137,20 +139,19 @@ while start_point < audible_length:
         plt.savefig('imgout/test' + str(count) + '.png')
         plt.close()
 
-    found_midi = {}
-
-    hz_max = numpy.argmax(hz_plot)
-    if hz_max > 0:
-        if silent:
+    found_midi = {} # list of found notes and their explanations
+    hz_max = numpy.argmax(hz_plot) # max frequency
+    if hz_max > 0: # if there's any volume in this frame
+        if silent: # and it was silent before, cap the silence
             timewise.startSilence = start_seconds
         silent = False
         
     # TODO: FUCKIN MAKE THIS BETTER
-        for x in range(0, 10):
-            hz_max = numpy.argmax(hz_plot)
-            max_midi = freqToMidi(hz_max) # get midi of it
-            #hz_plot[hz_max] = 0 # flip this to 0 so it's not max anymore
+        for x in range(0, NUM_PEAKS):
+            hz_max = numpy.argmax(hz_plot) # max frequency, again
+            max_midi = freqToMidi(hz_max) # get midi o
 
+            # run the code to explain what function the max pitch was
             code, cur_midi = timewise.explain(max_midi, start_seconds, end_seconds)
 
             # if it doesn't know why, cur_midi will be None, set it manually
@@ -197,7 +198,7 @@ while start_point < audible_length:
             #   found_hz.append(hz_max)
             #   found_midi.append(cur_midi)
 
-    print("For quarter:", str(count), "(" + str(start_seconds), "to", str(end_seconds) + "): Notes found:", found_midi)
+    print("For window:", str(count), "(" + str(start_seconds), "to", str(end_seconds) + "): Notes found:", found_midi)
 
     events[start_point / sample_rate] = found_midi
 
@@ -210,4 +211,4 @@ pp.pprint(sorted(events.items(), key=operator.itemgetter(0)))
 print("Expected:")
 pp.pprint(timewise.offsets)
 
-addTimeline(timewise, mei_file, stem)
+addTimeline(timewise, mei_file, new_file_stem)
